fde3175cce218373392a46bf0d196b6ce3971e76
Nika <Nika@users-MacBook-Pro.local>
2025-01-09 19:16:15 +0400

    feat: add scoring logic and UI for players and draws

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -6,6 +6,9 @@ const TicTacToe = () => {
   const [winner, setWinner] = useState<string | null>(null);
   const [winningLine, setWinningLine] = useState<number[] | null>(null);
   const [turn, setTurn] = useState<number>(0);
+  const [xScore, setXScore] = useState<number>(0);
+  const [drawScore, setDrawScore] = useState<number>(0);
+  const [yScore, setYScore] = useState<number>(0);
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,38 +34,82 @@ const TicTacToe = () => {
       const newBoard = [...board];
       newBoard[index] = currentPlayer;
       setBoard(newBoard);
+
       const result = checkWinner(newBoard);
+
       if (result) {
         setWinner(result);
+        if (result === "x") {
+          setXScore((x) => x + 1);
+        } else if (result === "o") {
+          setYScore((y) => y + 1);
+        }
+      } else if (turn + 1 === board.length) {
+        setWinner("draw");
+        setDrawScore((draw) => draw + 1);
       } else {
         setTurn((prev) => prev + 1);
         setCurrentPlayer(currentPlayer === "x" ? "o" : "x");
       }
     }
-    if (turn === board.length - 1) {
-      setWinner("draw");
-    }
+  };
+
+  const newGame = () => {
+    setBoard(Array(9).fill(""));
+    setCurrentPlayer("x");
+    setWinner(null);
+    setWinningLine(null);
+    setTurn(0);
   };
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-        gap: "5px",
-      }}
-    >
-      {board.map((cell, index) => (
-        <button
-          key={index}
-          onClick={() => {
-            handleClick(index);
-          }}
-          className={winningLine?.includes(index) ? "winning-cell" : ""}
-        >
-          {cell}
+    <div className="wrapper">
+      <div className="board">
+        <div className="gameWrapper">
+          <div className="resultsContainer">
+            <div className="result playerX">
+              <p className="player">Player X </p>
+              <span className="score">{xScore}</span>
+            </div>
+            <div className="result draw">
+              <p className="player">Draw </p>
+              <span className="score">{drawScore}</span>
+            </div>
+            <div className="result playerO">
+              <p className="player">Player O </p>
+              <span className="score">{yScore}</span>
+            </div>
+          </div>
+          <div className="gridContainer">
+            {board.map((cell, index) => (
+              <div
+                key={`${cell}-${index}`}
+                className={`cell ${
+                  winningLine?.includes(index) ? "winning-cell" : ""
+                } ${winner === "draw" && "draw"}`}
+                onClick={() => {
+                  handleClick(index);
+                }}
+              >
+                <button
+                  key={index}
+                  onClick={() => {
+                    handleClick(index);
+                  }}
+                  className={`cellButton ${
+                    cell ? `cell${cell.toUpperCase()}` : ""
+                  }`}
+                >
+                  {cell}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <button className="newGame" onClick={newGame}>
+          New Game
         </button>
-      ))}
+      </div>
     </div>
   );
 };
